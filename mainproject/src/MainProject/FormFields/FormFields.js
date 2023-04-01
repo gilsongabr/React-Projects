c369f64cf8495f5bd22869d3a9247b49358579fc
Gilson Gabriel <gabrielhc_hc@hotmail.com>
2023-03-31 21:08:02 -0400

    Alteração no input

--- mainproject/src/MainProject/FormFields/FormFields.js
+++ mainproject/src/MainProject/FormFields/FormFields.js
@@ -1,6 +1,16 @@
+import './FormFields.css'
+// import Form from 'react-bootstrap/Form'
 function FormFields(props) {
-    // console.log('~> ', props)
-    return <div>fieldId = {props.fieldId} e fieldName = {props.fieldName} </div>
+    // console.log(props)
+    return <div className="FormLabel" style={{width: props.fieldWidth ? props.fieldWidth.toString() + '%' : "10%"}}>
+        {props.fieldName}
+        <div className="FormField">
+            <input className="inputClass" />
+        </div>
+        <div>
+            fieldId = {props.fieldId}
+        </div>
+    </div>
 }
 
 export default FormFields
