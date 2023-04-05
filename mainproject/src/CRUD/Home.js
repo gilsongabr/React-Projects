10b363776267908639c755fb15208e35675de666
Gilson Gabriel <gabrielhc_hc@hotmail.com>
2023-04-04 22:48:51 -0400

    Estudando a utilização de chaning functions

--- mainproject/src/CRUD/Home.js
+++ mainproject/src/CRUD/Home.js
@@ -1,8 +1,12 @@
-import React from "react";
 import FormFields from "../MainProject/FormFields/FormFields";
 
 export default class Home extends FormFields{
+
     render() {
+        this.field
+            .addName('Nome')
+            .addNumber(90)
+        console.log('this.record ~> ', this)
         return super.render({ a: 'TESTE ', b: ' HERANÇA ', c:' DE REACT!'})
     }
 }
